ea128704b3dfa6bfe344041efe3a7d7d8a500ddd
tanzimJUCSE <mahfuz.bipu96@gmail.com>
2020-04-26 01:42:13 +0600

    Matrix Sort based on index

---
+++ Matrix Sort based on column number(Nested List)/sort.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+    rows = [input() for _ in range(N)]
+    K = int(input())
+
+    for row in sorted(rows, key=lambda row: int(row.split()[K])):
+        print(row)
