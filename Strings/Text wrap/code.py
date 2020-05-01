9daf106cdb6ec16561b27d11a356af2254bb259f
tanzimJUCSE <mahfuz.bipu96@gmail.com>
2020-05-01 17:07:01 +0600

    String Updated

--- Strings/Text wrap/code.py
+++ Strings/Text wrap/code.py
@@ -2,3 +2,5 @@ import textwrap
 s = input()
 w = int(input().strip())
 print(textwrap.fill(s,w))
+
+    
---
+++ Strings/capitalize() title().py
@@ -0,0 +1,5 @@
+s="tanzim mahfuz"
+for x in s.split():
+    print(x.capitalize())
+    
+#or we can use the input().title() method
